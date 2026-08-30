3793bce3ad8f990f3f3ddf057b61c37661902a92
yrizzz <rekberariez@gmail.com>
2026-08-30 14:34:00 +0700

    fix: horizontal topbar inherits navbar-color theme (text, border, hover)

--- resources/views/partials/topbar-horizontal.blade.php
+++ resources/views/partials/topbar-horizontal.blade.php
@@ -5,6 +5,8 @@
 @endphp
 
 <div x-show="$store.ui.layout === 'horizontal'" x-cloak
+     data-navbar-color="{{ $_COOKIE['ak_nb_color'] ?? 'default' }}"
+     :data-navbar-color="$store.ui.navbarColor"
      class="sticky top-16 z-20 hidden border-b border-border bg-background/80 backdrop-blur-lg lg:block">
     <div
         x-data="{
